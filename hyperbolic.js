4c17eb24df92d06a8660b534ee4940125f48b771
Matt <total_garbage_inovernivneriv8934@mailinator.com>
2016-01-18 21:40:28 +1100

    Initial import: OCR using a neural network in JavaScript.

---
+++ hyperbolic.js
@@ -0,0 +1,40 @@
+/**
+ * Polyfills for hyperbolic functions
+ */
+
+Math.sinh = Math.sinh || function(x) {
+	var y = Math.exp(x);
+	return (y - 1 / y) / 2;
+};
+
+Math.cosh = Math.cosh || function(x) {
+	var y = Math.exp(x);
+	return (y + 1 / y) / 2;
+};
+
+Math.tanh = Math.tanh || function(x) {
+	if (x === Infinity) {
+		return 1;
+	} else if (x === -Infinity) {
+		return -1;
+	} else {
+		var y = Math.exp(2 * x);
+		return (y - 1) / (y + 1);
+	}
+};
+
+Math.asinh = Math.asinh || function(x) {
+	if (x === -Infinity) {
+		return x;
+	} else {
+		return Math.log(x + Math.sqrt(x * x + 1));
+	}
+};
+
+Math.acosh = Math.acosh || function(x) {
+	return Math.log(x + Math.sqrt(x * x - 1));
+};
+
+Math.atanh = Math.atanh || function(x) {
+	return Math.log((1 + x) / (1 - x)) / 2;
+};
---
+++ ocr.js
@@ -0,0 +1,132 @@
+if (typeof(OCR) === "undefined") {
+	OCR = {};
+}
+
+OCR.NodeBase = function() {};
+$.extend(OCR.NodeBase, {});
+
+OCR.Node = function() {
+	OCR.NodeBase.apply(this, arguments);
+	this.inputs = [];
+};
+OCR.Node.prototype = new OCR.NodeBase();
+$.extend(OCR.Node.prototype, {
+	addInput: function(edge) {
+		this.inputs.push(edge);
+		edge.output = this;
+	},
+	addOutput: function(edge) {
+		this.outputs.push(edge);
+		edge.input = this;
+	},
+	getValue: function() {
+		var weightedSum = 0;
+		$.each(this.inputs, function(i, edge) {
+			weightedSum += edge.input.getValue() * edge.weight;
+		});
+		return Math.tanh(weightedSum);
+	}
+});
+
+OCR.Edge = function(weight) {
+	this.weight = weight || this.defaultWeight();
+};
+$.extend(OCR.Edge.prototype, {
+	defaultWeight: function() {
+		return; // TODO
+	}
+});
+
+OCR.LayerBase = function(numNodes) {
+	this.nodes = [];
+	while (numNodes-- > 0) {
+		this.nodes.push(new OCR.Node());
+	}
+}
+$.extend(OCR.LayerBase.prototype, {
+	connectOutputsToLayer: function(toLayer) {
+		$.each(this.nodes, function(i, fromNode) {
+			$.each(toLayer.nodes, function(i, toNode) {
+				var edge = new OCR.Edge();
+				fromNode.addOutput(edge);
+				toNode.addInput(edge);
+			});
+		});
+	}
+});
+
+OCR.Layer = function(numNodes) {
+	OCR.LayerBase.apply(this, arguments);
+};
+OCR.Layer.prototype = new OCR.LayerBase();
+$.extend(OCR.Layer.prototype, {
+});
+
+OCR.InputLayer = function(width, height) {
+	this.width = width;
+	this.height = height;
+	OCR.LayerBase.call(this, width * height);
+};
+OCR.InputLayer.prototype = new OCR.LayerBase();
+$.extend(OCR.InputLayer.prototype, {
+	connectImageToInputs: function(image) {
+		// TODO
+	}
+});
+
+OCR.Network = function(image, options) {
+	$.extend(this, this.defaultOptions, options);
+	this.image = image;
+	this.outputLayer = new OCR.Layer(this.hiddenLayerSize);
+	this.hiddenLayer = new OCR.Layer(this.image.width * this.image.height);
+	this.hiddenLayer.connectOutputsToLayer(this.outputLayer);
+	this.inputLayer = new OCR.InputLayer(this.inputLayerSizeSqrt, this.inputLayerSizeSqrt);
+	this.inputLayer.connectImageToInputs(this.image);
+	this.inputLayer.connectOutputsToLayer(this.hiddenLayer);
+};
+$.extend(OCR.Network.prototype, {
+	defaultOptions: {
+		inputLayerSizeSqrt: 5,
+		hiddenLayerSize: 10,
+		learningRate: 0.5
+	},
+	learn: function() {
+		var inputImage;
+		var outputs;
+		var errors;
+		var totalError = 0;
+		$.each(errors, function(i, error) {
+			errors[i] = outputs[i] - correct[i];
+		});
+		$.each(errors, function(i, error) {
+			totalError += error * error;
+		});
+		totalError *= 0.5;
+	}
+});
+
+OCR.ActivationFunc = {
+	"tanh": {
+		"func": Math.tanh,
+		"derivative": function(input) {
+			var r = Math.tanh(input);
+			return 1 - (r * r);
+		}
+	},
+	"logistic": {
+		"func": function(input) {
+			return 1 / (1 + Math.pow(-input));
+		},
+		"derivative": function(input) {
+			return; /* TODO */
+		}
+	},
+};
+OCR.ActivationFunc['default'] = OCR.ActivationFunc['tanh'];
+
+(function($) {
+	$(function() {
+		var image = $('img.original');
+		var net = new OCR.Network(image);
+	});
+})(jQuery);
